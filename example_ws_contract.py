fa352b53003ff97cf04fa4c86a87a182ffbe9e1c
davidh180 <dah180@protonmail.com>
2022-11-26 20:46:56 -0800

    Initial commit

---
+++ example_ws_contract.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+from bitget.consts import CONTRACT_WS_URL
+from bitget.ws.bitget_ws_client import BitgetWsClient, SubscribeReq
+
+
+def handle(message):
+    print("handle:" + message)
+
+
+def handel_error(message):
+    print("handle_error:" + message)
+
+
+def handel_btcusd(message):
+    print("handel_btcusd:" + message)
+
+
+if __name__ == '__main__':
+    api_key = ""
+    secret_key = ""
+    passphrase = ""
+    symbol = 'btcusd'
+
+    client = BitgetWsClient(CONTRACT_WS_URL, need_login=True) \
+        .api_key(api_key) \
+        .api_secret_key(secret_key) \
+        .passphrase(passphrase) \
+        .error_listener(handel_error) \
+        .build()
+
+    channles = [SubscribeReq("mc", "ticker", "BTCUSD"), SubscribeReq("SP", "candle1W", "BTCUSDT")]
+    client.subscribe(channles,handle)
+
+    channles = [SubscribeReq("mc", "ticker", "ETHUSD")]
+    client.subscribe(channles, handel_btcusd)
